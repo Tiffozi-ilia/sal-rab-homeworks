060178d835d511e20224174cb69ac3ee5914420c
Ilia <ilia@local>
2022-08-18 14:20:58 +0300

    task_3 amended v2

--- task_3/solution.js
+++ task_3/solution.js
@@ -1,14 +1,13 @@
 function renderCartItem(item) {
     const product = item;
-    product = {id: 0, title: "Пицца", count: 10, price: 100}
     let itemCountText = count * price;
-    itemCountText = ``;
+    let itemCountText = " ";
     itemCountText = itemCountText + product.count;
-    itemCountText = itemCountText + " x ";
+    itemCountText = itemCountText + " × ";
     itemCountText = itemCountText + product.price;
     itemCountText = itemCountText + " ₽ = ";
     let sum = product.count * product.price
-    itemCountText = itemCountText + + sum
+    itemCountText = itemCountText + sum
     itemCountText = itemCountText + " ₽";
 
     // Задание №3.1. Формирование строки корзины
